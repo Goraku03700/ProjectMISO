22ef47083859239df752ac9c006066f89db3e37a
Goraku03700 <goraku.like@gmail.com>
2016-09-16 10:29:30 +0900

    sample

---
+++ MISO/Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public  Rigidbody rigidbody;
+    public float acceleration;
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        
+
+        // xとzに10をかけて押す力をアップ
+        rigidbody.AddForce(x * acceleration, 0, z * acceleration);
+	}
+}
