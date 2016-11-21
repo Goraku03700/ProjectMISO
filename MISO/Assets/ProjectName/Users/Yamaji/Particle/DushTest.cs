4514ab7741812616968192e82bcadcc95d2f1ba1
yamasiy <seya2134@gmail.com>
2016-11-21 10:18:19 +0900

    プルのため

---
+++ MISO/Assets/ProjectName/Users/Yamaji/Particle/DushTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DushTest : MonoBehaviour {
+
+   
+    private ParticleSystem[] m_particles;
+
+    [SerializeField]
+    private ParticleSystem m_particleRun;
+
+    [SerializeField]
+    private int m_particleMax; 
+
+    // Use this for initialization
+	void Start () {
+
+        for(int i=0; i<m_particleMax; i++)
+        {
+
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
