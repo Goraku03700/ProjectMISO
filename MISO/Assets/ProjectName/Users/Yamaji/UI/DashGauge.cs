5fc0b71db6fe5b48425c061ef657bf248d85f25b
yamasiy <seya2134@gmail.com>
2016-11-25 09:41:04 +0900

    セレクト更新

--- MISO/Assets/ProjectName/Users/Yamaji/UI/DashGauge.cs
+++ MISO/Assets/ProjectName/Users/Yamaji/UI/DashGauge.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class DashGauge : MonoBehaviour {
 
+    [SerializeField]
+    private Camera m_targetCamera;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +13,7 @@ public class DashGauge : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        //カメラの方向を向くようにする。
+        this.transform.LookAt(this.m_targetCamera.transform.position);
+    }
 }
